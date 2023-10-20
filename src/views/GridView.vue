8b3dda73886d4f499fcebfafab30e39a4d3e411d
Deng Tao <d17381844023@gmail.com>
2023-10-20 16:31:55 +0800

    update

--- src/views/GridView.vue
+++ src/views/GridView.vue
@@ -1,18 +1,24 @@
 <template>
-<div class="w-full h-full border border-red-500 grid-container p-2 relative">
-	<div class="absolute -top-10 space-x-2">
-		<button
-			v-for="btn in btns"
-			:key="btn.label"
-			@click="btn.onClick"
-			class="p-1 border border-gray-500 rounded"
-		>
-			{{ btn.label }}
-		</button>
-	</div>
+<div class="absolute space-x-2 name--button-group">
+	<button
+		v-for="btn in btns"
+		:key="btn.label"
+		@click="btn.onClick"
+		class="p-1 border border-gray-500 rounded"
+	>
+		{{ btn.label }}
+	</button>
+</div>
+<div
+	class="relative w-full h-full p-2 border border-red-500 grid-container"
+	:style="{
+		'gridTemplateRows': `repeat(${rows},1fr)`,
+		'gridTemplateColumns': `repeat(${cols},1fr)`
+	}"
+>
 	<div
 		ref="itemsRef"
-		v-for="item, index in items"
+		v-for="item, index in layout"
 		:key="index"
 		class="border border-blue-400 grid-item"
 		:data-static="item.static"
@@ -24,51 +30,67 @@
 		}"
 		@click="selectItem(item)"
 	>
-		{{ item.i }}
+		<slot
+			name=content
+			:data="item"
+		>
+			{{ item.i }}
+		</slot>
 	</div>
 </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watchEffect, shallowRef, computed } from 'vue';
-import { useKeyModifier } from '@vueuse/core'
-const items = ref([
-	{ i: 1, w: 1, h: 1, static: false, select: false, position: null },
-	{ i: 2, w: 2, h: 3, static: true, select: false, position: null },
+import { ref, shallowRef, computed, defineExpose } from 'vue';
+import { useKeyModifier } from '@vueuse/core';
+const btns = [
+	{ label: '合并', onClick: () => { console.log('合并'); merge(); } },
+	{ label: '拆分', onClick: () => { console.log('拆分'); split(); } },
+	{ label: '删除', onClick: () => { console.log('删除') } },
+];
+
+const rows = ref(3);
+const cols = ref(4);
+const layout = ref([
+	{ i: 0, w: 1, h: 1, static: false, select: false, position: null },
+	{ i: 1, w: 2, h: 2, static: true, select: false, position: null },
+	{ i: 2, w: 1, h: 1, static: false, select: false, position: null },
 	{ i: 3, w: 1, h: 1, static: false, select: false, position: null },
 	{ i: 4, w: 1, h: 1, static: false, select: false, position: null },
 	{ i: 5, w: 1, h: 1, static: false, select: false, position: null },
 	{ i: 6, w: 1, h: 1, static: false, select: false, position: null },
 	{ i: 7, w: 1, h: 1, static: false, select: false, position: null },
-	{ i: 8, w: 1, h: 1, static: false, select: false, position: null },
-	{ i: 9, w: 1, h: 1, static: false, select: false, position: null },
-	{ i: 10, w: 1, h: 1, static: false, select: false, position: null },
-	{ i: 11, w: 1, h: 1, static: false, select: false, position: null },
+	{ i: 8, w: 1, h: 1, static: false, select: false, position: null }
 ]);
 
-const selected = computed(() => items.value.filter(item => item.select));
+const colLayoutMap = {
+	0: [0, 3, 5],
+	3: [3, 5],
+	2: [2, 4, 8],
+	4: [4, 8],
+};
 
-const isControled = useKeyModifier('Control');
+
+const selected = computed(() => layout.value.filter(item => item.select));
+
+const onControl = useKeyModifier('Control');
 
 function selectItem(item: any) {
-	if (isControled.value && !item.static) {
+	if (onControl.value && !item.static) {
 		item.select = !item.select;
 	}
 }
-const itemsRef = shallowRef<HTMLElement[]>(null);
 
-const btns = [
-	{ label: '合并', onClick: () => { console.log('合并'); handlerMerge(); } },
-	{ label: '拆分', onClick: () => { console.log('拆分') } },
-	{ label: '删除', onClick: () => { console.log('删除') } },
-]
+
+
+const itemsRef = shallowRef<HTMLElement[]>(null);
 
 function calcPos() {
 	itemsRef.value?.forEach(el => {
 		const dataset = el.dataset;
 		if (dataset.static === 'true') return;
 		const id = dataset.id;
-		const item = items.value.find(item => String(item.i) === id);
+		const item = layout.value.find(item => String(item.i) === id);
 		item.position = el.getBoundingClientRect();
 	})
 }
@@ -78,7 +100,8 @@ function inRow(items = selected.value) {
 		if (index === 0) {
 			return pre;
 		} else {
-			return pre && cur.position.top === array[index - 1].position.top;
+			return pre && cur.position.top === array[index - 1].position.top
+				&& cur.position.height === array[index - 1].position.height;
 		}
 	}, true)
 };
@@ -87,12 +110,12 @@ function inCol(items = selected.value) {
 		if (index === 0) {
 			return pre;
 		} else {
-			return pre && cur.position.left === array[index - 1].position.left;
+			return pre && cur.position.left === array[index - 1].position.left
+				&& cur.position.width === array[index - 1].position.width;
 		}
 	}, true);
 };
-function isNested(items = selected.value) {
-	const rowDirect = inRow(items);
+function isNested(rowDirect: boolean, colDirect: boolean) {
 	if (rowDirect) {
 		return selected.value.reduce((pre, cur, index, array) => {
 			if (index === 0) {
@@ -102,7 +125,6 @@ function isNested(items = selected.value) {
 			}
 		}, true)
 	}
-	const colDirect = inCol(items);
 	if (colDirect) {
 		return selected.value.reduce((pre, cur, index, array) => {
 			if (index === 0) {
@@ -114,14 +136,83 @@ function isNested(items = selected.value) {
 	}
 }
 
-function handlerMerge() {
+function merge() {
 	calcPos();
-	console.log('handlerMerge', items.value);
-	console.log('isNested', isNested());
+	const isInRow = inRow();
+	const isInCol = inCol();
+	const nested = isNested(isInRow, isInCol);
+	if (isInRow && nested) {
+		const firstSelected = selected.value?.[0];
+		firstSelected.w = selected.value?.reduce((pre, cur) => {
+			return pre + cur.w
+		}, 0);
+
+		selected.value?.forEach((v, index) => {
+			if (index === 0) return;
+			const i = layout.value?.findIndex(i => i === v);
+			if (i > -1) {
+				console.log('find i', i);
+				layout.value?.splice(i, 1);
+			}
+		});
+	}
+	if (isInCol && nested) {
+		const firstSelected = selected.value?.[0];
+		firstSelected.h = selected.value?.reduce((pre, cur) => {
+			return pre + cur.h
+		}, 0);
+
+		selected.value?.forEach((v, index) => {
+			if (index === 0) return;
+			const i = layout.value?.findIndex(i => i === v);
+			if (i > -1) {
+				console.log('find i', i);
+				layout.value?.splice(i, 1);
+			}
+		});
+	}
 }
-watchEffect(() => {
-	console.log("itemsRef", itemsRef.value, itemsRef.value?.map((item) => item.dataset));
-	console.log("itemsRef not static", itemsRef.value?.filter((item) => item.dataset.static === 'false'));
+
+function split() {
+	const canSplit = selected.value?.length === 1;
+	const splitItem = { ...selected.value?.[0] };
+	if (canSplit) {
+		const isRowSplit = splitItem.w > 1;
+		if (isRowSplit) {
+			const splitItemIndex = splitItem.i;
+			Array(splitItem.w)
+				.fill(0)
+				.forEach((_, index) => {
+					console.log('forEach', index);
+					layout.value?.splice(splitItemIndex + index, index === 0 ? 1 : 0, {
+						...splitItem,
+						w: 1,
+						i: splitItem.i + index
+					})
+				})
+		}
+		const isColSplit = splitItem.h > 1;
+		if (isColSplit) {
+			const splitItemIndex = splitItem.i;
+			const colLayout: number[] = colLayoutMap[splitItemIndex];
+			const splitCandidate = colLayout.slice(0, splitItem.h);
+			splitCandidate.forEach(i => {
+				layout.value?.splice(i, i === splitItemIndex ? 1 : 0, {
+					...splitItem,
+					h: 1,
+					i: i
+				})
+			})
+		}
+	}
+	layout.value?.forEach(item => {
+		item.select = false;
+	})
+}
+
+defineExpose({
+	merge,
+	split
 })
 
 </script>
